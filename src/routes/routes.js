ca36a8b8fd1167d17e174b660ae35861e9154704
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-06-20 14:37:55 +0300

    changement de routes en songroutes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,8 @@
 import e from "express";
 import { addSongs, checkQueryParams, getSong } from "../services/Song.js";
 
-export const routes = e.Router()
+export const song_routes = e.Router()
 
-routes.get('/songs', checkQueryParams, getSong)
+song_routes.get('/get', checkQueryParams, getSong)
 
-routes.post('/songs', addSongs)
+song_routes.post('/', addSongs)
