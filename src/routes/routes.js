62ab3237710e795bed6522695c36c2d28818e9a6
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-06-20 14:28:19 +0300

    changement de méthode d'export

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,8 @@
 import e from "express";
 import { addSongs, checkQueryParams, getSong } from "../services/Song.js";
 
-const routes = e.Router()
+export const routes = e.Router()
 
 routes.get('/songs', checkQueryParams, getSong)
 
 routes.post('/songs', addSongs)
-
-export default routes
