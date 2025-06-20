a74472739ad22d41d169a57675c1fb5c456edb59
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-06-20 14:32:33 +0300

    amélioration du modèle de donnée pour les chansons

--- src/models/Song.js
+++ src/models/Song.js
@@ -1,20 +1,13 @@
 import mongoose from "mongoose";
 
 const songsSchema = mongoose.Schema({
-    // titre du chanson
     titre : { type: String, required: true, default: "" },
-    // taille du fichier en Mo
     taille : { type: Number, required: false, default: 0 },
-    // durée du chanson en seconde
     duree : { type: Number, required: false, default: 0 },
-    // chanteur/chanteuse du chanson
     auteur : { type: String, required: true, default: "" },
-    // emplacement du fichier
     fichier_url : { type: String, required: true, default: "" },
-    // album titulaire du chanson
-    album : { type: String, required: false, default: "" }
+    album : { type: String, required: false, default: "" },
+    add_date : { type: Date, required: true, default: Date.now }
 })
 
-const ChansonCollection = new mongoose.model("Chansons", songsSchema);
-
-export default ChansonCollection
+export const Song = new mongoose.model("Songs", songsSchema);
