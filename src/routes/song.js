32836c0796d128edf02cc541f16eab72e9630fc5
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-01-17 15:27:00 +0300

    suppression de ligne inutile

--- src/routes/song.js
+++ src/routes/song.js
@@ -4,5 +4,4 @@ import { addSong, checkQueryParams, getSong } from "../services/song.js";
 export const song_router = Router()
 
 song_router.get('/get', checkQueryParams, getSong)
-
 song_router.post('/post', addSong)
