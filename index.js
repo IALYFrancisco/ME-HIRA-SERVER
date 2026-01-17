9161fb5c17852c569a351a26fdfa7a79b975c263
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-01-17 15:04:50 +0300

    amélioration encore du configuration CORS de l'application

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ import e from 'express'
 
 import dotenv from 'dotenv'
 import { app_router } from './src/routes/index.js'
+import { cors_configuration } from './src/services/cors.js'
 
 dotenv.config()
 
 const app = e()
 
 app.use(e.json())
+
+app.use(cors_configuration)
 
 app.use(app_router)
 
--- src/services/cors.js
+++ src/services/cors.js
@@ -1,5 +1,5 @@
 export const cors_configuration = (request, response, next) => {
-    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Origin', process.env.ALLOWED_HOST);
     response.setHeader('Access-Control-Allow-Headers', '*');
     response.setHeader('Access-Control-Allow-Methods', 'GET,POST')
     next()
